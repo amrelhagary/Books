7db754ca1380d7fe0c54298535faa4865ea7bea8
Amr Elhagary <amr.elhagary@gmail.com>
2017-08-11 00:54:18 -0500

    fix bug in handling exception

--- src/BookBundle/EventListener/ExceptionalListener.php
+++ src/BookBundle/EventListener/ExceptionalListener.php
@@ -4,32 +4,37 @@ namespace BookBundle\EventListener;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class ExceptionalListener
 {
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
-        $message = sprintf(
-            'Invalid API Request: %s',
-            $exception->getMessage()
+        $path = $event->getRequest()->getPathInfo();
+        if($exception->getStatusCode() == 405 && strpos($path, '/api') !== false)
+        {
+            $message = sprintf(
+                'Invalid API Request: %s',
+                $exception->getMessage()
             );
 
-        // Customize your response object to display the exception details
-        $response = new Response();
-        $response->headers->set('Content-Type', 'application/json');
-        $response->setContent(json_encode(['status' => 'error', 'message' => $message]));
+            // Customize your response object to display the exception details
+            $response = new Response();
+            $response->headers->set('Content-Type', 'application/json');
+            $response->setContent(json_encode(['status' => 'error', 'message' => $message]));
 
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            // HttpExceptionInterface is a special type of exception that
+            // holds status code and header details
+            if ($exception instanceof HttpExceptionInterface) {
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
+            } else {
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+            // Send the modified response object to the event
+            $event->setResponse($response);
         }
-
-        // Send the modified response object to the event
-        $event->setResponse($response);
     }
 }
